80916c8d8c051cd1b5467a62eafb3a960fd52368
Evtim Lyonov <evtim.lyonov@gmail.com>
2021-05-31 17:27:30 +0300

    Update mail.js

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,8 +1,10 @@
 document.getElementById('formid').addEventListener('submit', function (e) {
     e.preventDefault();
-    const subject = document.getElementById('subject').value;
-    const text = document.getElementById('text').value;
-    window.open(`mailto:evtim.lyonov@gmail.com?subject=${subject}&body=${text}`);
+    window.open(`mailto:evtim.lyonov@gmail.com?subject=${document.getElementById('subject').value}&body=${document.getElementById('text').value}`);
     document.getElementById('message-sent').style.display = 'block';
     document.getElementById('sent-email').style.display = 'none';
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('subject').value = '';
+    document.getElementById('text').value = '';
 });
