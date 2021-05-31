e896a7a6899070f1dfc5046270bcb10e5198df63
Evtim Lyonov <evtim.lyonov@gmail.com>
2021-05-31 17:39:33 +0300

    Update mail.js

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -3,6 +3,15 @@ document.getElementById('formid').addEventListener('submit', function (e) {
     window.open(`mailto:evtim.lyonov@gmail.com?subject=${document.getElementById('subject').value}&body=${document.getElementById('text').value}`);
     document.getElementById('message-sent').style.display = 'block';
     document.getElementById('sent-email').style.display = 'none';
+    document.querySelectorAll('input').forEach(input => {
+        input.value = '';
+        input.disabled = 'true';
+    })
+    const text = document.getElementById('text');
+    text.value = '';
+    text.disabled = 'true';
+
+    /* 
     const name = document.getElementById('name');
     const email = document.getElementById('email');
     const subject = document.getElementById('subject');
@@ -16,4 +25,5 @@ document.getElementById('formid').addEventListener('submit', function (e) {
     subject.disabled = 'true';
     text.value = '';
     text.disabled = 'true';
+    */
 });
