736a85fb23666fa3b7a2f0647d7e57015a7a849d
Evtim Lyonov <evtim.lyonov@gmail.com>
2021-05-31 17:09:15 +0300

    Update mail.js

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -2,7 +2,7 @@ document.getElementById('formid').addEventListener('submit', function (e) {
     e.preventDefault();
     const subject = document.getElementById('subject').value;
     const text = document.getElementById('text').value;
-    window.open(`mailto:c0ol.dude@yahoo.com?subject=${subject}&body=${text}`);
+    window.open(`mailto:evtim.lyonov@gmail.com?subject=${subject}&body=${text}`);
     document.getElementById('message-sent').style.display = 'block';
     document.getElementById('sent-email').style.display = 'none';
 });
