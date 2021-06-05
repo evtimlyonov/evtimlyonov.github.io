c3b79351cb2d4088e752252ac5cb7fad45945524
Evtim Lyonov <evtim.lyonov@gmail.com>
2021-06-05 15:29:13 +0300

    Update toggle.js

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,8 +1,4 @@
 function toggle(section) {
     let x = document.getElementById(section);
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+    x.style.display = x.style.display === 'none' ? 'block' : 'none';
 }
